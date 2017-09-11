f5ab3cdc314a1b757385f7f1421cd95d67901074
TBaudon <baudon.thomas@gmail.com>
2017-09-11 02:39:38 +0200

    router and config

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 [![Build Status](https://travis-ci.org/Pokeet/headless-api.svg?branch=master)](https://travis-ci.org/Pokeet/headless-api)
+
+api response format : http://jsonapi.org
--- src/main.js
+++ src/main.js
@@ -1,8 +1,67 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import expressValidator from 'express-validator';
+
+import mongoose from 'mongoose';
+
+import config from '../config';
+
+import routes from './routes';
 
 const app = express();
 
-app.use(bodyParser);
+mongoose.Promise = global.Promise;
+
+// connect to db
+
+const uri = `mongodb://${config.dbSettings.host}/${config.dbSettings.name}`;
+
+mongoose.connect(uri)
+  .then(() => console.log('Connected to db with success'))
+  .catch(err => console.error(err));
+
+const db = mongoose.connection;
+
+// middlewares
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false,
+}));
+
+app.use(expressValidator({
+  customValidators: {
+  },
+}));
+
+// routing
+
+app.use(`/api/${config.apiVersion}`, routes);
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err,
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {},
+  });
+});
 
 app.listen(3000);
+
+module.exports = app;

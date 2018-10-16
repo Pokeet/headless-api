62bf20ba7e7bd7b342314d565e54358972635ec8
Thomas Baudon <thomas.baudon@pokeet.fr>
2018-10-16 22:49:34 +0200

    updated version message

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,15 @@ import express from 'express'
 import ModelsController from './controllers/ModelsController'
 import UserController from './controllers/UserController'
 
+import Package from '../package.json'
+
 const router = express.Router()
 
 router.use('/models', ModelsController)
 router.use('/users', UserController)
 
 router.get('/', (req, res) => {
-  res.end('hi')
+  res.end('pokeet headless-api version : ' + Package.version)
 })
 
 module.exports = router
